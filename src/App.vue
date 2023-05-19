7efda4bec1af2a8ed54c28787e5c55df62a45b7e
evelyncaraujo <evyaraujoifc@gmail.com>
2023-05-19 11:58:25 -0300

    cc

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { livros } from '@/_data/livros.js'
+  </script>
+
+<template>
+  <h1> livraria (de novo)</h1>
+  <div class="listagem-livros">
+    <div class="card-livro" v-for="livro in livros" :key="livro.id">
+      <img src="livro.img" alt="capa do livro" class="capa-livro" />
+      <p class="titulo-livros">{{ livros.title }}</p>
+      <p class="titulo-autor">{{ livros.author }}</p>
+      <p class="titulo-preco">{{ livros.price }}</p>
+     <!-- {{ livro}} -->
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+h1{
+  color: black;
+  
+}
+.listagem-livros{
+  display: flex;
+  flex-wrap: wrap;
+
+}
+.capa-livro{
+  width: 250px;
+  padding: 10px;
+  background-color: beige;
+  border-radius: 5px;
+}
+</style>
---
+++ src/_data/livros.js
@@ -0,0 +1,53 @@
+export const livros = [
+    {
+      id: 1,
+      title: 'O poderoso chefão',
+      author: 'Mario Puzo',
+      rice: 49.9 ,
+      img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fproduto.mercadolivre.com.br%2FMLB-2127312902-livro-o-labirinto-do-fauno-capa-dura-editora-intrinseca-_JM&psig=AOvVaw2bUTbJj8nBoTLXhHcejl3i&ust=1684594113420000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICxz5_Qgf8CFQAAAAAdAAAAABAI'
+      
+    },
+    {
+      id: 2,
+      title: 'O senhor dos anéis',
+      author: 'hjdfhdjh',
+      rice: 69.9,
+      img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fproduto.mercadolivre.com.br%2FMLB-2127312902-livro-o-labirinto-do-fauno-capa-dura-editora-intrinseca-_JM&psig=AOvVaw2bUTbJj8nBoTLXhHcejl3i&ust=1684594113420000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICxz5_Qgf8CFQAAAAAdAAAAABAI'
+
+    },
+    {
+      id: 3,
+      title: 'O conde de monte Cristo',
+      author: 'Alexandre dumas',
+      rice: 59.9,
+      img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fproduto.mercadolivre.com.br%2FMLB-2127312902-livro-o-labirinto-do-fauno-capa-dura-editora-intrinseca-_JM&psig=AOvVaw2bUTbJj8nBoTLXhHcejl3i&ust=1684594113420000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICxz5_Qgf8CFQAAAAAdAAAAABAI'
+
+    },
+    {
+      id: 4,
+      title: 'O hobbit',
+      author: 'J.R.R.Tolkien',
+      rice: 49.9 ,
+      img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fproduto.mercadolivre.com.br%2FMLB-2127312902-livro-o-labirinto-do-fauno-capa-dura-editora-intrinseca-_JM&psig=AOvVaw2bUTbJj8nBoTLXhHcejl3i&ust=1684594113420000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICxz5_Qgf8CFQAAAAAdAAAAABAI'
+
+    },
+    {
+      id: 5,
+      title: 'A culpa é das estrelas',
+      author: 'Jhon Green',
+      rice: 49.9,
+      img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fproduto.mercadolivre.com.br%2FMLB-2127312902-livro-o-labirinto-do-fauno-capa-dura-editora-intrinseca-_JM&psig=AOvVaw2bUTbJj8nBoTLXhHcejl3i&ust=1684594113420000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICxz5_Qgf8CFQAAAAAdAAAAABAI'
+
+    },
+    {
+      id: 6,
+      title: 'Como eu era antes de você',
+      author: 'Jojo Moyes',
+      rice: 69.9 ,
+      img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fproduto.mercadolivre.com.br%2FMLB-2127312902-livro-o-labirinto-do-fauno-capa-dura-editora-intrinseca-_JM&psig=AOvVaw2bUTbJj8nBoTLXhHcejl3i&ust=1684594113420000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICxz5_Qgf8CFQAAAAAdAAAAABAI'
+
+  
+    }
+  
+]
+
